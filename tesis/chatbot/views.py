0533ebbb7ebdb30153c1da9a0d9a8a10fd300af1
José Rojas <josea.rcampero@gmail.com>
2024-06-22 21:48:07 -0400

    Actualizada app del chatbot y rutas de la api

--- tesis/chatbot/views.py
+++ tesis/chatbot/views.py
@@ -1,11 +1,11 @@
 import json
 from django.http import JsonResponse
-from rest_framework import viewsets
+from rest_framework.views import APIView
 
 from tesis.chatbot.gemini_model import model, prompt
 
 
-class ChatbotView(viewsets.ViewSet):
+class ChatbotView(APIView):
 
     def post(self, request, *args, **kwargs):
         # Parse JSON data
