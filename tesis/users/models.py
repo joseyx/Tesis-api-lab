3b8ddd715cabb5ff4e298b7d83f252fa35862a3d
José Rojas <josea.rcampero@gmail.com>
2024-06-22 21:46:45 -0400

    Creada app de usuarios

---
+++ tesis/users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    ROLES = [
+        ('cliente', 'User'),
+        ('admin', 'Administrator'),
+        # Add more roles as needed
+    ]
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    username = None
+
+    phone = models.CharField(max_length=13, null=True, blank=True)
+    role = models.CharField(max_length=10, choices=ROLES, default='cliente')
+    user_image = models.ImageField(upload_to='user_images/', null=True, blank=True, default='default_user_image.png')
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # New: related_name for ManyToMany fields
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='tesis_user_groups',  # Custom related_name for groups
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='tesis_user_permissions',  # Custom related_name for permissions
+        blank=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
---
+++ tesis/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import RegisterView, LoginView, UserView, LogoutView, UserViewApi, UserViewApiDetail
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('users', UserViewApi.as_view()),
+    path('users/<int:id>', UserViewApiDetail.as_view())
+]
