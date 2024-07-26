bc2357647b3fe3e826dea6c91e42b90e17131e03
José Rojas <josea.rcampero@gmail.com>
2024-07-26 03:08:02 -0400

    corregidos ajustes

--- tesis/settings.py
+++ tesis/settings.py
@@ -144,15 +144,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
 ]
 
-"""
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jm.calderon1105@gmail.com'
-EMAIL_HOST_PASSWORD = 'gjed fmmi jevu opcf'
-DEFAULT_FROM_EMAIL = 'jm.calderon1105@gmail.com'
-"""
+
 # Email configuration
 EMAIL_BACKEND = env("EMAIL_BACKEND")
 EMAIL_HOST = env("EMAIL_HOST")
